068da94a2230ae707f59a50ef91babbf9a115353
crialmar <cristina.almagon@gmail.com>
2024-05-03 10:29:50 +0200

    enrutado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 export const Header = () => {
@@ -5,30 +6,30 @@ export const Header = () => {
     <>
       <div className="header__container">
         <div className="nav__header">
-          <a className="home">
+          <Link to="/" className="nav_link">
             <img
               src="https://cdn-icons-png.flaticon.com/512/25/25694.png"
               className="nav-home"
             />
-          </a>
-          <a className="home">
+          </Link>
+          <Link to="/aboutme" className="nav_link">
             <img
               src="https://cdn.icon-icons.com/icons2/1369/PNG/512/-person_90382.png"
               className="nav-about-me"
             />
-          </a>
-          <a className="home">
+          </Link>
+          <Link to="/proyects" className="nav_link">
             <img
               src="https://cdn-icons-png.flaticon.com/512/6208/6208597.png"
               className="nav-proyects"
             />
-          </a>
-          <a className="home">
+          </Link>
+          <Link to="/cv" className="nav_link">
             <img
               src="https://cdn-icons-png.flaticon.com/512/3846/3846805.png"
-              className="nav-about-me"
+              className="nav-cv"
             />
-          </a>
+          </Link>
         </div>
       </div>
     </>
