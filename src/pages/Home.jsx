a770e0d82b631fedf32dd14e4dda54b4da202300
crialmar <cristina.almagon@gmail.com>
2024-05-06 19:47:37 +0200

    cc2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,13 @@
 export const Home = () => {
-  return <div>home</div>;
+  return (
+    <div className="home__container">
+      <div className="title">
+        <h1>Buenas!! Me llamo Cris</h1>
+      </div>
+      <div className="title2">
+        <h2>Soy Full-Stack junior y este es </h2>
+        <h2>mi portfolio</h2>
+      </div>
+    </div>
+  );
 };
