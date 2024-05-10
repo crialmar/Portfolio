1b4aeba25bac02152e52c7bd933aa8dd2288e46e
crialmar <cristina.almagon@gmail.com>
2024-05-10 10:42:34 +0200

    h2

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,3 +1,16 @@
 export const AboutMe = () => {
-  return <div>soy yooo la que sigue aqui</div>;
+  return (
+    <div className="about-me">
+      <h1>ABOUT ME</h1>
+      <h2>
+        Tecnologías
+        <h5>JavaScript</h5>
+        <h5>CSS</h5>
+        <h5>ReactJS</h5>
+        <h5>NodeJS</h5>
+        <h5>TypeScript</h5>
+        <h5>Python</h5>
+      </h2>
+    </div>
+  );
 };
--- src/pages/Proyects.jsx
+++ src/pages/Proyects.jsx
@@ -1,3 +1,10 @@
 export const Proyects = () => {
-  return <div>proyects</div>;
+  return (
+    <div className="proyects">
+      <h1>PROYECTOS</h1>
+      <h2>Daruma</h2>
+      <h2>Oye Vecino!</h2>
+      <h2>To do list</h2>
+    </div>
+  );
 };
